6bbd66c8721c357105b19cbfb9f3375aba9fe98e
enkelad <ibortsov14@gmail.com>
2018-12-15 20:49:02 +0300

    Add the Posts controller
    Add the Posts controller with few actions for testing Router class.

---
+++ App/Controllers/Posts.php
@@ -0,0 +1,14 @@
+<?php
+
+class Posts
+{
+    public function index()
+    {
+        echo 'The index page of the Posts controller.';
+    }
+
+    public function addNew()
+    {
+        echo 'Add new post in the Posts controller.';
+    }
+}
